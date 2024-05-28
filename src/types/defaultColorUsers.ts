63e7ff17b79a733fced4c3f8c401b7d794d9fef2
Alexander Ramian <alexander.ramian@htw-dresden.de>
2024-05-28 19:04:31 +0200

    refactor: deleted some colors for more contrast and better visibility
    fix: deleted comma

--- src/types/defaultColorUsers.ts
+++ src/types/defaultColorUsers.ts
@@ -18,26 +18,18 @@ export const getColorForUser = (userId: string): string => {
 const colorMap: Map<string, string> = new Map<string, string>()
 const colors = [
 	"#f44336",
-	"#e91e63",
 	"#9c27b0",
 	"#673ab7",
 	"#3f51b5",
 	"#2196f3",
-	"#03a9f4",
 	"#00bcd4",
 	"#009688",
 	"#4caf50",
 	"#8bc34a",
 	"#cddc39",
 	"#ffeb3b",
-	"#ffc107",
-	"#ff9800",
-	"#ff5722",
 	"#795548",
 	"#9e9e9e",
 	"#607d8b",
-	"#ffffff",
-	"#000000"
-
 ]
 

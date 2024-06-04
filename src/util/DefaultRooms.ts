2b05596010a96a23a85a6e7ee3e8ac9623951e98
derikon <dev@dasbuntwerk.de>
2024-06-04 11:00:47 +0200

    chore: :bookmark: add rooms for the study

--- src/util/DefaultRooms.ts
+++ src/util/DefaultRooms.ts
@@ -1,76 +1,76 @@
 import { InteractionMode, Room } from "@sharedTypes/roomTypes";
 
 export const defaultRooms: Room[] = [
-// 	{
-// 		currentRecordingTime: 0,
-// 		participants: [],
-// 		description: "",
-// 		id: "9cbb9a45-b03c-4693-bf05-9cd47806ebca",
-// 		maxDurationRecord: 20000,
-// 		mode: InteractionMode.Jamming,
-// 		name: "Amsterdam",
-// 		recordingNamePrefix: "amsterdam"
-// 	},
-// 	{
-// 		currentRecordingTime: 0,
-// 		participants: [],
-// 		description: "",
-// 		id: "01bb6259-37dc-47a6-a58b-d1dafb18d2d4",
-// 		maxDurationRecord: 20000,
-// 		mode: InteractionMode.Jamming,
-// 		name: "Rotterdam",
-// 		recordingNamePrefix: "rotterdam"
-// 	},
-// 	{
-// 		currentRecordingTime: 0,
-// 		participants: [],
-// 		description: "",
-// 		id: "5443977d-0ef5-4acc-bd95-4c39753eb1c6",
-// 		maxDurationRecord: 20000,
-// 		mode: InteractionMode.Jamming,
-// 		name: "Den Haag",
-// 		recordingNamePrefix: "denHaag"
-// 	},
-// 	{
-// 		currentRecordingTime: 0,
-// 		participants: [],
-// 		description: "",
-// 		id: "48b735ca-ea21-4a67-a4ae-26ce90b3e31c",
-// 		maxDurationRecord: 20000,
-// 		mode: InteractionMode.Jamming,
-// 		name: "Utrecht",
-// 		recordingNamePrefix: "utrecht"
-// 	},
-// 	{
-// 		currentRecordingTime: 0,
-// 		participants: [],
-// 		description: "",
-// 		id: "6fc0ff7f-b7c6-459f-a24b-cb14392bd238",
-// 		maxDurationRecord: 20000,
-// 		mode: InteractionMode.Jamming,
-// 		name: "Eindhoven",
-// 		recordingNamePrefix: "eindhoven"
-// 	},
-// 	{
-// 		currentRecordingTime: 0,
-// 		participants: [],
-// 		description: "",
-// 		id: "105d1f69-bd3f-49f5-9615-cf7e8209e929",
-// 		maxDurationRecord: 20000,
-// 		mode: InteractionMode.Jamming,
-// 		name: "Groningen",
-// 		recordingNamePrefix: "groningen"
-// 	},
-// 	{
-// 		currentRecordingTime: 0,
-// 		participants: [],
-// 		description: "",
-// 		id: "4ec34f34-97f9-44b5-bc53-1a321a2288ee",
-// 		maxDurationRecord: 20000,
-// 		mode: InteractionMode.Jamming,
-// 		name: "Tilburg",
-// 		recordingNamePrefix: "tilburg"
-// 	},
+	// 	{
+	// 		currentRecordingTime: 0,
+	// 		participants: [],
+	// 		description: "",
+	// 		id: "9cbb9a45-b03c-4693-bf05-9cd47806ebca",
+	// 		maxDurationRecord: 20000,
+	// 		mode: InteractionMode.Jamming,
+	// 		name: "Amsterdam",
+	// 		recordingNamePrefix: "amsterdam"
+	// 	},
+	// 	{
+	// 		currentRecordingTime: 0,
+	// 		participants: [],
+	// 		description: "",
+	// 		id: "01bb6259-37dc-47a6-a58b-d1dafb18d2d4",
+	// 		maxDurationRecord: 20000,
+	// 		mode: InteractionMode.Jamming,
+	// 		name: "Rotterdam",
+	// 		recordingNamePrefix: "rotterdam"
+	// 	},
+	// 	{
+	// 		currentRecordingTime: 0,
+	// 		participants: [],
+	// 		description: "",
+	// 		id: "5443977d-0ef5-4acc-bd95-4c39753eb1c6",
+	// 		maxDurationRecord: 20000,
+	// 		mode: InteractionMode.Jamming,
+	// 		name: "Den Haag",
+	// 		recordingNamePrefix: "denHaag"
+	// 	},
+	// 	{
+	// 		currentRecordingTime: 0,
+	// 		participants: [],
+	// 		description: "",
+	// 		id: "48b735ca-ea21-4a67-a4ae-26ce90b3e31c",
+	// 		maxDurationRecord: 20000,
+	// 		mode: InteractionMode.Jamming,
+	// 		name: "Utrecht",
+	// 		recordingNamePrefix: "utrecht"
+	// 	},
+	// 	{
+	// 		currentRecordingTime: 0,
+	// 		participants: [],
+	// 		description: "",
+	// 		id: "6fc0ff7f-b7c6-459f-a24b-cb14392bd238",
+	// 		maxDurationRecord: 20000,
+	// 		mode: InteractionMode.Jamming,
+	// 		name: "Eindhoven",
+	// 		recordingNamePrefix: "eindhoven"
+	// 	},
+	// 	{
+	// 		currentRecordingTime: 0,
+	// 		participants: [],
+	// 		description: "",
+	// 		id: "105d1f69-bd3f-49f5-9615-cf7e8209e929",
+	// 		maxDurationRecord: 20000,
+	// 		mode: InteractionMode.Jamming,
+	// 		name: "Groningen",
+	// 		recordingNamePrefix: "groningen"
+	// 	},
+	// 	{
+	// 		currentRecordingTime: 0,
+	// 		participants: [],
+	// 		description: "",
+	// 		id: "4ec34f34-97f9-44b5-bc53-1a321a2288ee",
+	// 		maxDurationRecord: 20000,
+	// 		mode: InteractionMode.Jamming,
+	// 		name: "Tilburg",
+	// 		recordingNamePrefix: "tilburg"
+	// 	},
 	{
 		currentRecordingTime: 0,
 		participants: [],
@@ -90,5 +90,155 @@ export const defaultRooms: Room[] = [
 		mode: InteractionMode.Jamming,
 		name: "Demo 2",
 		recordingNamePrefix: "demo_2"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa11",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "Team 1",
+		recordingNamePrefix: "team_1"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa12",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "T1_P1",
+		recordingNamePrefix: "T1_P1"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa13",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "T1_P2",
+		recordingNamePrefix: "T1_P2"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa21",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "Team 2",
+		recordingNamePrefix: "team_2"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa22",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "T2_P1",
+		recordingNamePrefix: "T2_P1"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa23",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "T2_P2",
+		recordingNamePrefix: "T2_P2"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa31",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "Team 3",
+		recordingNamePrefix: "team_3"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa32",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "T3_P1",
+		recordingNamePrefix: "T3_P1"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa33",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "T3_P2",
+		recordingNamePrefix: "T3_P2"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa41",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "Team 4",
+		recordingNamePrefix: "team_4"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa42",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "T4_P1",
+		recordingNamePrefix: "T4_P1"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa43",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "T4_P2",
+		recordingNamePrefix: "T4_P2"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa51",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "Team 5",
+		recordingNamePrefix: "team_5"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa52",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "T5_P1",
+		recordingNamePrefix: "T5_P1"
+	},
+	{
+		currentRecordingTime: 0,
+		participants: [],
+		description: "",
+		id: "4ec34f34-97f9-44b5-bc53-1a321aaaaa53",
+		maxDurationRecord: 10000,
+		mode: InteractionMode.Jamming,
+		name: "T5_P2",
+		recordingNamePrefix: "T5_P2"
 	},
 ]
